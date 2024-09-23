463c84514c7a758c09b1534b5d791e4e7b3cec3d
Céline Mira <celinemira.pro@gmail.com>
2024-09-23 16:37:39 +0200

    mise a jour

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [],
   templateUrl: './panier.component.html',
-  styleUrl: './panier.component.css'
+  styleUrls: ['./panier.component.css'],
+  imports: [NgFor]
 })
-export class PanierComponent {
+export class PanierComponent implements OnInit {
+  cartItems: any[] = [];
 
+  ngOnInit(): void {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      this.cartItems = [JSON.parse(storedCart)];
+    }
+  }
+
+  getTotal(): number {
+    return this.cartItems.reduce((total, item) => total + item.total, 0);
+  }
 }
