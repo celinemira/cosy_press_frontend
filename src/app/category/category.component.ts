d5da2bc5e3cdf1637dfa082aca418c846c35826b
Céline Mira <celinemira.pro@gmail.com>
2024-09-24 13:01:36 +0200

    Mise a jour

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrls: ['./category.component.css']
 })
 export class CategoryComponent {
 
+  constructor(private router: Router) { }
+
+  goToArticle(name: string, price: number, img: string): void {
+    this.router.navigate(['/article', { name, price, img }]);
+  }
 }
--- src/app/page-article/page-article.component.ts
+++ src/app/page-article/page-article.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './page-article.component.html',
-  styleUrl: './page-article.component.css'
+  styleUrls: ['./page-article.component.css']
 })
-export class PageArticleComponent {
+export class PageArticleComponent implements OnInit {
+  lavage: boolean = true;
+  repassage: boolean = true;
+  quantity: number = 1;
+  unitPrice: number = 0; // Ce sera mis à jour avec les données de la catégorie
+  depositDate: string = '';
 
+  itemName: string = ''; // Nom de l'article
+  itemImage: string = ''; // Image de l'article
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.itemName = params['name'];
+      this.unitPrice = +params['price'];
+      this.itemImage = params['img'];
+    });
+  }
+
+  setOption(option: string, value: boolean): void {
+    if (option === 'lavage') {
+      this.lavage = value;
+    } else if (option === 'repassage') {
+      this.repassage = value;
+    }
+  }
+
+  changeQuantity(amount: number): void {
+    if (this.quantity + amount > 0) {
+      this.quantity += amount;
+    }
+  }
+
+  addToCart(): void {
+    // Logique d'ajout au panier
+    const cartItem = {
+      name: this.itemName,
+      lavage: this.lavage,
+      repassage: this.repassage,
+      quantity: this.quantity,
+      total: this.quantity * this.unitPrice,
+      date: this.depositDate,
+      image: this.itemImage
+    };
+    localStorage.setItem('cart', JSON.stringify(cartItem));
+    alert("Article ajouté au panier avec succès !");
+  }
 }
